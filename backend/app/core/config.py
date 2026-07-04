9a76bbb44cface3f7955a5193bb520f86af29d06
JrVolt <andreapallinilavoro@gmail.com>
2026-07-04 21:52:36 +0200

    :)

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -188,7 +188,7 @@ class Settings(BaseSettings):
     backup_s3_secret_key: str = ""
 
     app_name: str = "PrintStash"
-    app_version: str = "0.8.1"
+    app_version: str = "0.8.2"
 
     @property
     def incoming_dir(self) -> Path:
